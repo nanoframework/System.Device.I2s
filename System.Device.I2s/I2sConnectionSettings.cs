1fcebd6d2158ba7c9e16d7d9ea8153d8c90badb1
Ibrahim KIVANC <ikivanc@hotmail.com>
2021-09-24 17:54:46 +0300

    remove unused parameters

--- System.Device.I2s/I2sConnectionSettings.cs
+++ System.Device.I2s/I2sConnectionSettings.cs
@@ -12,23 +12,17 @@ namespace System.Device.I2s
     /// </summary>
     public sealed class I2sConnectionSettings
     {
-
-        private I2sMode _i2sMode = I2sMode.Pdm;
+        private int _busId = 1;
 
         private int _sampleRate = 44_100;
+
+        private I2sMode _i2sMode = I2sMode.Pdm;
 
         private I2sBitsPerSample _i2sBitsPerSample = I2sBitsPerSample.Bit16;
 
         private I2sChannelFormat _i2sChannelFormat = I2sChannelFormat.RightLeft;
 
         private I2sCommunicationFormat _i2sConnectionFormat = I2sCommunicationFormat.StandI2s;
-
-        // default intrrupt priority
-        private int _allocFlags = 0;  
-
-        private int _bufferCount = 8;
-
-        private int _bufferLength = 16;
 
         /// <summary>
         /// Initializes new instance of I2sConnectionSettings.
@@ -42,25 +36,19 @@ namespace System.Device.I2s
         /// Initializes a new instance of the <see cref="I2sConnectionSettings"/> class.
         /// </summary>
         /// <param name="busId">The bus ID the device is connected to.</param>
-        /// <param name="chipSelectLine">The chip select line used on the bus. Optional, -1 if not used</param>
-        public I2sConnectionSettings(int busId, int chipSelectLine = -1)
+        public I2sConnectionSettings(int busId)
         {
             BusId = busId;
-            ChipSelectLine = chipSelectLine;
         }
 
         internal I2sConnectionSettings(I2sConnectionSettings other)
         {
             BusId = other.BusId;
-            ChipSelectLine = other.ChipSelectLine;
             Mode = other.Mode;
             SampleRate = other.SampleRate;
             CommunicationFormat = other.CommunicationFormat;
             ChannelFormat = other.ChannelFormat;
-            BufferLength = other.BufferLength;
-            BufferCount = other.BufferCount;
             BitsPerSample = other.BitsPerSample;
-            AllocFlags = other.AllocFlags;
         }
 
         /// <summary>
@@ -73,19 +61,6 @@ namespace System.Device.I2s
             set
             {
                 _busId = value;
-            }
-        }
-
-        /// <summary>
-        /// The chip select line used on the bus.
-        /// </summary>
-        public int ChipSelectLine
-        {
-            get => _csLine;
-
-            set
-            {
-                _csLine = value;
             }
         }
 
@@ -118,7 +93,7 @@ namespace System.Device.I2s
         /// <summary>
         /// I2S Channel Format
         /// </summary>
-        public int ChannelFormat
+        public I2sChannelFormat ChannelFormat
         {
             get => _i2sChannelFormat;
 
@@ -142,32 +117,6 @@ namespace System.Device.I2s
         }
 
         /// <summary>
-        /// Buffer count.
-        /// </summary>
-        public int BufferCount
-        {
-            get => _bufferCount;
-
-            set
-            {
-                _bufferCount = value;
-            }
-        }
-
-        /// <summary>
-        /// Buffer lenght.
-        /// </summary>
-        public int BufferLength
-        {
-            get => _bufferLength;
-
-            set
-            {
-                _bufferLength = value;
-            }
-        }
-
-        /// <summary>
         /// Sample Rate.
         /// </summary>
         public int SampleRate
@@ -177,19 +126,6 @@ namespace System.Device.I2s
             set
             {
                 _sampleRate = value;
-            }
-        }
-
-        /// <summary>
-        /// Alloc Flags
-        /// </summary>
-        public int AllocFlags
-        {
-            get => _allocFlags;
-
-            set
-            {
-                _allocFlags = value;
             }
         }
     }
